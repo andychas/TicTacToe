6c611477135e786799889e8dc90fa389eb07f2dc
andychas <chas.andrey@gmail.com>
2015-07-16 14:35:30 +0300

    commit

---
+++ TicTacToe.WebSite/App_Code/IService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+[ServiceContract(CallbackContract = typeof(ICallBack), SessionMode = SessionMode.Required)]
+
+// Calls from client to Server
+public interface IService
+{
+
+    [OperationContract(IsOneWay = true)]
+    void GetData(int x, int y);
+}
+
+// Return Values from Server to Client
+public interface ICallBack
+{
+    [OperationContract(IsOneWay = true)] // void is not enough
+    void Result(int res);
+}
---
+++ TicTacToe.WebSite/App_Code/Service.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+public class Service : IService
+{
+    public void GetData(int x, int y)
+    {
+        int res = x + y;
+        ICallBack channel = OperationContext.Current.GetCallbackChannel<ICallBack>();
+        channel.Result(res);
+
+    }
+}
