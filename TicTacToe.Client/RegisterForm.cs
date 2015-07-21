b0c752d750d0c5d0a17a169850558bba6f067127
dvirs <dvir.sofer@walla.com>
2015-07-21 12:21:31 +0300

    test

---
+++ TicTacToe.Client/RegisterForm.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe.Client
+{
+    public partial class RegisterForm : Form
+    {
+        public RegisterForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SubmitBtn_Click(object sender, EventArgs e)
+        {
+            GameInfoForm gameInfo = new GameInfoForm();
+            gameInfo.Show();
+            //test
+
+        }
+    }
+}
