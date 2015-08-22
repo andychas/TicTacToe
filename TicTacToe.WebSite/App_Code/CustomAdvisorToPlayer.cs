087a295355e9ecd7f1ff41c258592afed9e49fee
dvirs <dvir.sofer@walla.com>
2015-08-22 16:45:03 +0300

    add custom

---
+++ TicTacToe.WebSite/App_Code/CustomAdvisorToPlayer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CustomAdvisorToPlayer
+{
+    public string advisorName { get; set; }
+    public string playerName { get; set; }
+}
